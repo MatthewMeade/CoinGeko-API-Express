55421ff20b206d3014ea630c263e012fed3bd8e0
Matthew Meade <mafumeade@gmail.com>
2019-01-13 21:16:21 -0330

    Add waiting

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-var request = require("request-promise");
+const request = require("request-promise");
+const Timeout = require("await-timeout");
 
 const cheerio = require("cheerio");
 
 let $;
 let lastGET;
+let loading = false;
 
 const updateData = async () => {
   loading = true;
+  console.log("LOADING");
   const body = await request("https://coinmarketcap.com/all/views/all/");
+  console.log("LOADED");
   $ = cheerio.load(body);
   lastGET = Date.now();
+  loading = false;
 };
 
 const ensureData = async () => {
+  console.log("Testing. Is loading?", loading);
+  if (loading) {
+    while (loading) {
+      console.log("Already loading, waiting");
+      await Timeout.set(100);
+    }
+    return;
+  }
+
   if (!$ || Date.now() - lastGET > 60000) await updateData();
 };
 
 app.get("/historical/:name", async (req, res) => {
   await ensureData();
 
-  const hour = $(`#id-${req.params.name} td[data-timespan='1h']`).attr(
-    "data-percentusd"
+  const hour = parseFloat(
+    $(`#id-${req.params.name} td[data-timespan='1h']`).attr("data-percentusd")
   );
-  const day = $(`#id-${req.params.name} td[data-timespan='24h']`).attr(
-    "data-percentusd"
+  const day = parseFloat(
+    $(`#id-${req.params.name} td[data-timespan='24h']`).attr("data-percentusd")
   );
-  const week = $(`#id-${req.params.name} td[data-timespan='7d']`).attr(
-    "data-percentusd"
+  const week = parseFloat(
+    $(`#id-${req.params.name} td[data-timespan='7d']`).attr("data-percentusd")
   );
 
-  res.send({ data: [hour, day, week].map(parseFloat) });
+  res.send({ day, hour, week });
 });
 
 app.get("/price/:name", async (req, res) => {
